9c0601cda00d908226e58123a0f2731f0baef259
Capt Daags <bytesailor@protonmail.ch>
2018-09-17 22:12:15 +1000

    Update NodeUnoAU.ino
    Ver 4.3 which has a simple LED implementation added to light a LED when it enters and exits a TX cycle. Handy when testing as it shows when you should be getting a packet on the TTN web site.

--- code/NodeUnoAU.ino
+++ code/NodeUnoAU.ino
@@ -1,41 +1,50 @@
-// MIT License
-// https://github.com/gonzalocasas/arduino-uno-dragino-lorawan/blob/master/LICENSE
-// Based on examples from https://github.com/matthijskooijman/arduino-lmic
-// Copyright (c) 2015 Thomas Telkamp and Matthijs Kooijman
-
-// Adaptions: Andreas Spiess
-//          : Darrin Pearce - Was throwing compile errors until I changed
-//                               LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
-//                            to LMIC_setSession (0x1, DEVADDR, (uint8_t*)NWKSKEY, (uint8_t*)APPSKEY);
-//                            which seemed to make sense even though I had no idea what I was doing
-//                            this has yet to be actually uploaded and tested so may be bollocks.
-//                            Source of this wisdom: https://github.com/things4u/LoRa-LMIC-1.51/issues/13
-//                            As you can see, like keeping orginal stuff in REM's in case I fuck it up.
-//            Darrin Pearce - Now operational, had to put back the PROGMEM stuff in the setup and also list out
-//                            the AU freq's to make sure we are TX'ing on the same as the gateway is listening.
-//                            The freq data was from the global in /opt/ttngateway/bin on the gateway Pi.
+/*******************************************************************************
+ * Copyright (c) 2015 Thomas Telkamp and Matthijs Kooijman
+ *
+ * Permission is hereby granted, free of charge, to anyone
+ * obtaining a copy of this document and accompanying files,
+ * to do whatever they want with them without any restriction,
+ * including, but not limited to, copying, modification and redistribution.
+ * NO WARRANTY OF ANY KIND IS PROVIDED.
+ *
+ * This example sends a valid LoRaWAN packet with payload "Hello,
+ * world!", using frequency and encryption settings matching those of
+ * the The Things Network.
+ *
+ * This uses ABP (Activation-by-personalisation), where a DevAddr and
+ * Session keys are preconfigured (unlike OTAA, where a DevEUI and
+ * application key is configured, while the DevAddr and session keys are
+ * assigned/generated in the over-the-air-activation procedure).
+ *
+ * Note: LoRaWAN per sub-band duty-cycle limitation is enforced (1% in
+ * g1, 0.1% in g2), but not the TTN fair usage policy (which is probably
+ * violated by this sketch when left running for longer)!
+ *
+ * To use this sketch, first register your application and device with
+ * the things network, to set or generate a DevAddr, NwkSKey and
+ * AppSKey. Each device should have their own unique values for these
+ * fields.
+ *
+ * Do not forget to define the radio type correctly in config.h.
+ *
+ *******************************************************************************/
+ // =====================================================
+ // REMEMBER TO RESET FRAME COUNTERS WHEN LOAD NEW SKETCH
+ // 4.3 Added code to turn on LED as its enter TX cycle
+ // and cleaned up REM'ed out stuff
+ // =====================================================
 
 #include <lmic.h>
 #include <hal/hal.h>
 #include <SPI.h>
 
-// ORIGINAL KEY DETAILS LEFT HERE AS AN EXAMPLE FOR WHEN YOU ADD YOURS
-// LoRaWAN NwkSKey, network session key
-// This is the default Semtech key, which is used by the early prototype TTN
-// network.
-// static const PROGMEM u1_t NWKSKEY[16] = { 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C };
-
-// LoRaWAN AppSKey, application session key
-// This is the default Semtech key, which is used by the early prototype TTN
-// network.
-// static const u1_t PROGMEM APPSKEY[16] = { 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C };
-
-// LoRaWAN end-device address (DevAddr)
-// static const u4_t DEVADDR = 0x03FF0001 ; // <-- Change this address for every node!
-
+// Specific To Each User And Device
 static const PROGMEM u1_t NWKSKEY[16] = YOUR KEY HERE;
 static const PROGMEM u1_t APPSKEY[16] = YOUR KEY HERE;
-static const PROGMEM u4_t DEVADDR = 0xYOUR KEY HERE;
+static const PROGMEM u4_t DEVADDR = 0x YOUR KEY HERE;
+
+// Define what port the LED will be sitting on
+#define MYLED 3
 
 // These callbacks are only used in over-the-air activation, so they are
 // left empty here (we cannot leave them out completely unless
@@ -44,12 +53,12 @@ void os_getArtEui (u1_t* buf) { }
 void os_getDevEui (u1_t* buf) { }
 void os_getDevKey (u1_t* buf) { }
 
-static uint8_t mydata[] = "Dummy Data";
+static uint8_t mydata[] = "Sketch V4.3";
 static osjob_t sendjob;
 
 // Schedule TX every this many seconds (might become longer due to duty
 // cycle limitations).
-const unsigned TX_INTERVAL = 300;
+const unsigned TX_INTERVAL = 60;
 
 // Pin mapping Dragino Shield
 const lmic_pinmap lmic_pins = {
@@ -101,6 +110,7 @@ void onEvent (ev_t ev) {
             }
             // Schedule next transmission
             os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
+            digitalWrite(MYLED, LOW);
             break;
         case EV_LOST_TSYNC:
             Serial.println(F("EV_LOST_TSYNC"));
@@ -125,6 +135,7 @@ void onEvent (ev_t ev) {
 }
 
 void do_send(osjob_t* j){
+  
     // Check if there is not a current TX/RX job running
     if (LMIC.opmode & OP_TXRXPEND) {
         Serial.println(F("OP_TXRXPEND, not sending"));
@@ -133,12 +144,16 @@ void do_send(osjob_t* j){
         LMIC_setTxData2(1, mydata, sizeof(mydata)-1, 0);
         Serial.println(F("Packet queued"));
     }
+    digitalWrite(MYLED, HIGH);
     // Next TX is scheduled after TX_COMPLETE event.
 }
 
 void setup() {
     Serial.begin(115200);
     Serial.println(F("Starting"));
+
+    pinMode(MYLED, OUTPUT);
+    digitalWrite(MYLED, HIGH);
 
     #ifdef VCC_ENABLE
     // For Pinoccio Scout boards
@@ -152,12 +167,12 @@ void setup() {
     // Reset the MAC state. Session and pending data transfers will be discarded.
     LMIC_reset();
 
-    // Set static session parameters. Instead of dynamically establishing a session
-    // by joining the network, precomputed session parameters are be provided.
-    #ifdef PROGMEM
+    // Set static session parameters.
     // On AVR, these values are stored in flash and only copied to RAM
     // once. Copy them to a temporary buffer here, LMIC_setSession will
     // copy them into a buffer of its own again.
+
+    #ifdef PROGMEM
     uint8_t appskey[sizeof(APPSKEY)];
     uint8_t nwkskey[sizeof(NWKSKEY)];
     memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
@@ -168,18 +183,8 @@ void setup() {
     LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
     #endif
 
-    #if defined(CFG_eu868)
-    // Set up the channels used by the Things Network, which corresponds
-    // to the defaults of most gateways. Without this, only three base
-    // channels from the LoRaWAN specification are used, which certainly
-    // works, so it is good for debugging, but can overload those
-    // frequencies, so be sure to configure the full frequency range of
-    // your network here (unless your network autoconfigures them).
-    // Setting up channels should happen after LMIC_setSession, as that
-    // configures the minimal channel set.
-    // NA-US channels 0-71 are configured automatically
-    //
-    // NOTE: THIS IS THE AUSTRALIAN AU915 CHANNEL PLAN
+    // Set up the channels used in Australia
+    
     LMIC_setupChannel(0, 916800000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      //
     LMIC_setupChannel(1, 917000000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI);      //
     LMIC_setupChannel(2, 917200000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      //
@@ -189,22 +194,7 @@ void setup() {
     LMIC_setupChannel(6, 918000000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      //
     LMIC_setupChannel(7, 918200000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      //
     // LMIC_setupChannel(8, 917500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_MILLI);      // 
-    //
-    //
-    // TTN defines an additional channel at 869.525Mhz using SF9 for class B
-    // devices' ping slots. LMIC does not have an easy way to define set this
-    // frequency and support for class B is spotty and untested, so this
-    // frequency is not configured here.
-    #elif defined(CFG_us915)
-    // NA-US channels 0-71 are configured automatically
-    // but only one group of 8 should (a subband) should be active
-    // TTN recommends the second sub band, 1 in a zero based count.
-    // https://github.com/TheThingsNetwork/gateway-conf/blob/master/US-global_conf.json
-    //
-    // REM'ed below out as dont think we need if for AU915 channelisation
-    // LMIC_selectSubBand(1);
-    #endif
-
+ 
     // Disable link check validation
     LMIC_setLinkCheckMode(0);
 
